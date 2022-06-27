60cf18c6ac3bdfdba08f6b9693a0fbbc3592afdb
amandacorral07 <amandalewis0707@gmail.com>
2022-06-27 06:43:01 -0700

    Trying to add red car png to github

--- js/index.js
+++ js/index.js
@@ -92,7 +92,7 @@ class RedCar{
     
     render(){
         const redCar = new Image()
-            redCar.src= "images/redcar.png"
+            redCar.src= "images/Redcar.png"
             redCar.onload=()=>{
                 ctx.drawImage(redCar, this.x, this.y)
             }
